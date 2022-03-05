72ccb3bd08e767b0ea87a1c559f932ff141f30e1
Yasmine <yasmine.m.f@live.co.uk>
2022-03-05 13:55:33 +0400

    [FEAT] Clean up syntax errors

--- src/App.js
+++ src/App.js
@@ -32,86 +32,53 @@ function App() {
     });
   });
 
-  //checking map hover - 1
-  useEffect(() => {
-  if (!map.current) return;
-function initMap () => {
+  function initMap() {
     map.current.on('load', () => {
-    map.current.addSource('cbs', {
-    'type': 'geojson',
-    'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'
-    });
-
-    map.current.addLayer({
-      'id': 'cf',         // country-fills
-      'type': 'fill',
+      map.current.addSource('cbs', {
+      'type': 'geojson',
+      'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'
+      });
+  
+      map.current.addLayer({
+        'id': 'cf',         // country-fills
+        'type': 'fill',
+        'source': 'cbs',
+        'layout': {},
+        'paint': {
+        'fill-color': '#BFBAAD',
+        'fill-opacity': 0.1
+        }
+      });
+      
+      map.current.addLayer({
+      'id': 'cb',         // country-borders
+      'type': 'line',
       'source': 'cbs',
       'layout': {},
       'paint': {
-      'fill-color': '#BFBAAD',
-      'fill-opacity': 0.1
+      'line-color': '#BFBAAD',
+      'line-width': 1
       }
-    });
-    
-    map.current.addLayer({
-    'id': 'cb',         // country-borders
-    'type': 'line',
-    'source': 'cbs',
-    'layout': {},
-    'paint': {
-    'line-color': '#BFBAAD',
-    'line-width': 1
-    }
-    });
-
-    map.current.addLayer({
-      "id": "cfh",  // country-fills-hover",
-      "type": "fill",
-      "source": "cbs",
-      "layout": {},
-      "paint": {
-          "fill-color": "#BFBAAD",
-          "fill-opacity": 0.5
-      },
-      "filter": ["==", "name", ""]
-    });
- }
- 
- useEffect(() => 
- if (!map.current) return; 
- this.initMap()
- 
- )
-
-  // When the user moves their mouse over the page, we look for features
-  // at the mouse position (e.point) and within the states layer (states-fill).
-  // If a feature is found, then we'll update the filter in the state-fills-hover
-  // layer to only show that state, thus making a hover effect.
-  map.current.on("mousemove", function(e) {
-      var features = map.current.queryRenderedFeatures(e.point, { layers: ["cf"] });
-      
-      if (features.length) {
-          map.current.getCanvas().style.cursor = 'pointer';
-          map.current.setFilter("cfh", ["==", "name", features[0].properties.name]);
-          } else {
-          map.current.setFilter("cfh", ["==", "name", ""]);
-          map.current.getCanvas().style.cursor = '';
-      }
-  });
+      });
   
-  // Reset the state-fills-hover layer's filter when the mouse leaves the map
-  map.current.on("mouseout", function() {
-      map.current.getCanvas().style.cursor = 'auto';
-      map.current.setFilter("cfh", ["==", "name", ""]);
-  });
-  
-  map.current.on("click", function(e) {
-      var features = map.current.queryRenderedFeatures(e.point, { layers: ["cf"] });
-      if (features.length) {
-          console.log(e, features[0].properties.name);
-      }
-  });
-});
+      map.current.addLayer({
+        "id": "cfh",  // country-fills-hover",
+        "type": "fill",
+        "source": "cbs",
+        "layout": {},
+        "paint": {
+            "fill-color": "#BFBAAD",
+            "fill-opacity": 0.5
+        },
+        "filter": ["==", "name", ""]
+      });
+
+   
+  }
+
+  //checking map hover - 1
+  useEffect(() => {
+  if (!map.current) return;
 });
 //end of map hover
 
